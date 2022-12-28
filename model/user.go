61ba58fd886011dc8a5fb77fc1ff6ddbfbc14893
ThongTran <thongtran@teqnological.asia>
2022-12-28 10:01:12 +0700

    add table user_poll

--- model/user.go
+++ model/user.go
@@ -1,8 +1,9 @@
 package model
 
 import (
-	"gorm.io/gorm"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type User struct {
@@ -11,6 +12,8 @@ type User struct {
 	Username  string          `json:"username"`
 	Password  string          `json:"-"`
 	Polls     []Poll          `json:"polls"`
+	UserPolls []UserPoll      `json:"-"`
+	UserVotes []UserVote      `json:"-"`
 	CreatedAt time.Time       `json:"created_at"`
 	UpdatedAt time.Time       `json:"updated_at"`
 	DeletedAt *gorm.DeletedAt `json:"-"`
